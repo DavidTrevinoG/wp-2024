7c6cf4ded21b74bd004a13e7754f16d909a3c48c
David Treviño <2130059@upv.edu.mx>
2024-03-12 09:05:04 -0600

    Agregar SweetAlert

--- php/p2-universidad/views/listado_vehiculos.php
+++ php/p2-universidad/views/listado_vehiculos.php
@@ -15,6 +15,7 @@
 <head>
         <title>Listado Vehículos</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
     <br>
@@ -55,9 +56,8 @@
                         <a href="editar_vehiculo.php?id_vehiculo=<?php echo $row['id_vehiculo']; ?>" class="btn btn-secondary">
                             Editar
                         </a>
-                        <a href="../models/crud.php?eliminar_id_vehiculo=<?php echo $row['id_vehiculo']; ?>" class="btn btn-danger" name">
-                            Eliminar
-                        </a>
+                        <button onclick="mostrarSweet(<?php echo $row['id_vehiculo']; ?>)" class="btn btn-danger">Eliminar</button>
+                
                     </td>
                     </tr>
                     <?php
@@ -70,5 +70,22 @@
         <a href="agregar_vehiculo.php" class="btn btn-primary">Agregar Vehículo</a>
         <a href="../index.php" class="btn btn-info">Regresar</a>
     </div>
+    <script>
+        function mostrarSweet(id_vehiculo){
+            Swal.fire({
+                title: "¿Estás seguro?",
+                text: "No podrás revertirlo!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Delete"
+                }).then((result) => {
+                if (result.isConfirmed) { 
+                    window.location.href = "../models/crud.php?eliminar_id_vehiculo=" + id_vehiculo ;
+                }
+            });
+        }
+    </script>
 </body>
 </html>
