afd3f66be2ca78bd371bad7dae829c3167519320
David Treviño <2130059@upv.edu.mx>
2024-03-13 18:43:46 -0600

    change main tittle

--- php/p2-universidad/index.php
+++ php/p2-universidad/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-        <title>Práctica 1</title>
+        <title>Práctica 2</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <style>
         ul.menu {
